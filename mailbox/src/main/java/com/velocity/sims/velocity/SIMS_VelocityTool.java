5d0bea564275768f8d056072340d2fc26e700233
maisui007 <zhangjnmg@163.com>
2016-08-24 13:34:31 +0800

    velocity

---
+++ mailbox/src/main/java/com/velocity/sims/velocity/SIMS_VelocityTool.java
@@ -0,0 +1,10 @@
+package com.velocity.sims.velocity;
+
+/**
+ * Created by hnair20160706 on 2016/8/22.
+ */
+public class SIMS_VelocityTool {
+    public String data() {
+        return "data from Tool";
+    }
+}
