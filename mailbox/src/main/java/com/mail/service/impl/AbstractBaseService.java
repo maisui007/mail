9e091041e5cc5cfb6df712c4a4d0213233c457f7
maisui007 <!zjmgll66!>
2016-08-31 22:14:32 +0800

    邮件整体设计

---
+++ mailbox/src/main/java/com/mail/service/impl/AbstractBaseService.java
@@ -0,0 +1,10 @@
+package com.mail.service.impl;
+
+import com.mail.service.itf.IBaseService;
+import org.springframework.ui.Model;
+
+/**
+ * Created by gll on 2016/8/26.
+ */
+public class AbstractBaseService implements IBaseService<Model> {
+}
