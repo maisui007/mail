cf0b24f5784cb1838ad76c8d27ad4ce5745946c2
maisui007 <!zjmgll66!>
2016-08-31 22:23:25 +0800

    邮件整体设计

---
+++ mailbox/src/main/java/com/mail/itf/impl/MailEngineImpl.java
@@ -0,0 +1,10 @@
+//package com.mail.itf.impl;
+//
+//import com.mail.itf.IMailEngine;
+//import com.mail.itf.IMailManager;
+//
+///**
+// * Created by  on 2016/8/2.
+// */
+//public class MailEngineImpl implements IMailEngine {
+//}
